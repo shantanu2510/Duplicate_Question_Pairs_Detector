57be85d7c5a24660e757b98b094a3bbc7f5e2b08
Your Name <you@example.com>
2024-04-19 15:17:31 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import helper
+import pickle
+
+model = pickle.load(open('model.pkl','rb'))
+
+st.header('Duplicate Question Pairs')
+
+q1 = st.text_input('Enter question 1')
+q2 = st.text_input('Enter question 2')
+q3 = st.text_input('Enter question 3')
+
+if st.button('Find'):
+    queries = [helper.query_point_creator(q1, q2), 
+               helper.query_point_creator(q1, q3)]
+    results = [model.predict(query)[0] for query in queries]
+
+    if all(results):
+        st.header('Duplicate')
+    else:
+        st.header('Not Duplicate')
